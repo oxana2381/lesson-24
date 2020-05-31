983b3eca9e7cf7176e07743d1eaee0e7768c11c9
Oxana Ciobanu <oxanochka23@yahoo.com>
2020-05-31 17:24:11 +0300

    using fetch

---
+++ index.js
@@ -0,0 +1,14 @@
+fetch('https://api.nasa.gov/planetary/apod?api_key=AgAOKPwUaNaoMb7jMr4ETKQGGqlMtzRNGwSVaHNf')
+.then(response => response.json())
+.then(data => {
+    let image =document.getElementById('image');
+    image.src=data.url
+
+    let title = document.querySelector('h1');
+    title.innerText = data.title
+    let copyright = document.getElementById('copyright');
+    copyright.innerText = data.copyright
+   let explanation = document.getElementById('explanation')
+   explanation.innerText = data.explanation
+})
+
