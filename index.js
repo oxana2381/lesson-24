3a5f57d162337d3b8a9a6c601b3880076781158b
Oxana Ciobanu <oxanochka23@yahoo.com>
2020-06-03 13:39:20 +0300

    using fetch

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 fetch('https://api.nasa.gov/planetary/apod?api_key=AgAOKPwUaNaoMb7jMr4ETKQGGqlMtzRNGwSVaHNf')
 .then(response => response.json())
 .then(data => {
-    let image =document.getElementById('image');
-    image.src=data.url
+   // let image =document.getElementById('image');
+  // image.src=data.url
+
+    //let video = document.getElementById('video');
+    video.src = data.url;
+  
 
     let title = document.querySelector('h1');
     title.innerText = data.title
@@ -10,6 +14,23 @@ fetch('https://api.nasa.gov/planetary/apod?api_key=AgAOKPwUaNaoMb7jMr4ETKQGGqlMt
      
     let explanation = document.getElementById('explanation')
     explanation.innerText = data.explanation
+
+let  genericMediaElement;
+let  genericImageElement;
+
+
+    if(media_type = video ){
+        genericMediaElement=document.createElement ('iframe','video')
+       
+        
+
+     } else{
+       
+        genericImageElement= document.createElement('img','image')
+      
+       image.src=data.url
+
+    }
 
 
    if(data.copyright){
